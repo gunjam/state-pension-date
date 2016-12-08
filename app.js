dbe4f0400a6257cca9c045e81540b966d760732e
Glenn Knowles <glenn.knowles@dwp.gsi.gov.uk>
2016-12-08 09:22:15 +0000

    Removed Bunyan & UUID code commited in error

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-
-// Get logging framework installed ASAP
-const bunyan = require('bunyan');
-
-const log = bunyan.createLogger({
-	name: 'spa',
-	streams: [
-		{
-			level: 'info',
-			path: './spa.log'  // log ERROR and above to a file
-		}
-	]
-});
-
-log.info('SPA Application started');
-
-// UUID generator
-const uuidV1 = require('uuid/v1');
-
 const express = require('express');
 
 const getStateRetirementDate = require('./src/get-state-retirement-date/get-state-retirement-date');
@@ -33,14 +14,6 @@ app.get('/:dob/:gender', (request, response) => {
 	const dateOfBirth = request.params.dob;
 	const gender = request.params.gender;
 	const stateRetirementDate = getStateRetirementDate(dateOfBirth, gender);
-
-	const logEntry = {
-		txnId: uuidV1(),
-		dateOfBirth,
-		gender
-	};
-
-	log.info(`Data: ${JSON.stringify(logEntry)}`);
 
 	response.send(`Calculated retirement date is: ${stateRetirementDate}`);
 });
